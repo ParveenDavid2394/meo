9da3d1c24d342f13563d8a9f8f0cf6a3e497a27b
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-01 23:47:17 +0800

    added PeopleList component to display all users

---
+++ src/components/PeopleList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class PeopleList extends Component{
+    render(){
+        return(
+            <div>
+                Hi from People.js
+            </div>
+        )
+    }
+}
+
+export default PeopleList
