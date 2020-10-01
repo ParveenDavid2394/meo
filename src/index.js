c4cde5d72457c7457d78de8b3c48efb2c748cf9c
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-01 23:48:56 +0800

    imported essential files and created store and passed it as props to nested components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import App from './components/App';
 import Heading from './components/Heading'
+import rootReducer from './reducers/index'
+
+import {Provider} from 'react-redux'
+import {applyMiddleware, createStore} from 'redux'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <Heading />
-    </BrowserRouter>
+
+    <Provider store={createStoreWithMiddleware(rootReducer)}>
+        <BrowserRouter>
+            <Heading />
+            <Switch>
+                <Route exact strict path='/' component={App} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
