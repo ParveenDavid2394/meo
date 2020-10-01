37b1f3e7feb900030cbd4ab9c3a7e2eacf968178
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-01 23:48:24 +0800

    created rootreducer to combine all reducers at one place

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux'
+
+import people_reducer from './people_reducer'
+
+const rootReducer = combineReducers({
+    people: people_reducer
+})
+
+export default rootReducer
