04b840355620af95df1b6cb2b13fb46bfeb58f67
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-01 19:43:42 +0800

    imported heading into root folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import App from './components/App';
+import Heading from './components/Heading'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Heading />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
