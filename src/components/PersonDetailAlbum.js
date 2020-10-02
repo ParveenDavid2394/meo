a89d5dd8b83a0a30d060cc357eec8ef0ef0f2858
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-02 13:27:05 +0800

    receive props from parent component and display pic using designated css styles

---
+++ src/components/PersonDetailAlbum.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+class PersonDetailAlbum extends Component{
+
+    render(){
+        return(
+            <div className='detailAlbumsListContainer'>
+                {
+                    this.props.photos && this.props.photos.map(pic =>{
+                        return(
+                            <div className='detailSinglePicContainer' key={pic.cover}>
+                                <img src={`Albums/${this.props.name}/${pic.cover}.jpeg`} alt="" className='detailAlbumPic'/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default PersonDetailAlbum
