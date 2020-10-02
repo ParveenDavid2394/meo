8efccd5e296baba15acfa77777982ec3435106f9
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-02 12:44:13 +0800

    added methods to receive props and state and pass them to nested components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import PeopleList from './PeopleList'
+import {getPeople} from '../action/index'
 
 class App extends Component{
+
+    componentDidMount(){
+        this.props.getPeople()
+    }
+
     render(){
+
         return(
             <div>
-
+                <PeopleList people={this.props.allPeople} />
             </div>
         )
     }
 }
 
-export default App
+const mapStateToProps = state =>{
+    // console.log("++++++++++++++++++", state)
+    return{
+        allPeople: state.people.getPeople
+    }
+}
+
+// const mapDispatchToProps = dispatch =>{
+//     return{
+//         fetchPeople: 
+//     }
+// }
+
+export default connect(mapStateToProps,{getPeople})(App) 
