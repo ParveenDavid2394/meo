baa2eb42bba2b31b9e45b576ae7c06e4e934ba27
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-02 12:40:23 +0800

    added middleware and linked new component to root component

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import App from './components/App';
 import Heading from './components/Heading'
 import rootReducer from './reducers/index'
+import PersonDetail from './components/PersonDetail'
 
 import {Provider} from 'react-redux'
 import {applyMiddleware, createStore} from 'redux'
+import promiseMiddleware from 'redux-promise'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,6 +22,7 @@ ReactDOM.render(
             <Heading />
             <Switch>
                 <Route exact strict path='/' component={App} />
+                <Route exact strict path='/:name' component={PersonDetail} />
             </Switch>
         </BrowserRouter>
     </Provider>
