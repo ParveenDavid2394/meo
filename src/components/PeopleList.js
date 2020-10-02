9a3c2783ea922a316e572dc2482bd32302aa6942
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-02 12:51:13 +0800

    added JSX, methods to display users retrieve from db.json

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react'
+import {Link, withRouter} from 'react-router-dom'
 
 class PeopleList extends Component{
+
+    renderPeople = () =>{
+        if (this.props.people !== undefined) {
+            return this.props.people.map(person => {
+                return(
+                    <Link to={`/${person.name}`} key={person.id} className='singleProfilePicContainer'>
+                        <img src={`Albums/${person.name}/${person.profile_pic}.jpeg`} alt="" className='profilePic'/>
+                        
+                        <div>
+                            <h1 className='info'>{person.name}</h1>
+                            {
+                                person.online_status === 'on' && <img src="Albums/live.svg" alt="live" className='live'/>
+                            }
+                        </div>
+                    </Link>
+                )
+            })
+        }
+    }
+
     render(){
+        console.log('PeopleList.js', this.props.people)
         return(
-            <div>
-                Hi from People.js
+            <div className='allProfilePicContainer'>
+                {this.renderPeople()}
             </div>
         )
     }
