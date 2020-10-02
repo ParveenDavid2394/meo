26241e18d62ec538a92aad59c0bd6ecbc723d46c
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-02 12:41:03 +0800

    created fetch request for getPeople action and added new action to get person detail

--- src/action/index.js
+++ src/action/index.js
@@ -1,6 +1,20 @@
+
 export const getPeople = () =>{
+
+    const getPeople = fetch('http://localhost:3004/people')
+                        .then(response => response.json())
+
     return {
         type: 'GET_PEOPLE',
-        payload: 'Hey yo'
+        payload: getPeople
+    }
+}
+
+export const personDetail = (name) =>{
+    const getPeople = fetch(`http://localhost:3004/people?name=${name}`,    {method:'GET'}).then(response => response.json())
+
+    return{
+        type:'PERSON_DETAIL',
+        payload: getPeople
     }
 }
